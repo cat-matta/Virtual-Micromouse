73b6909aff21debe9a59b01e874de17045464499
Catherine Matta <48600733+cat-matta@users.noreply.github.com>
2020-07-13 22:14:55 -0400

    Changed name from test to main

--- script.sh
+++ script.sh
@@ -3,13 +3,13 @@ set ff=unix
 
 #this is to compile my files
 g++ -c -Wall -gstabs main.cpp
-g++ -Wall -gstabs main.o -o test
+g++ -Wall -gstabs main.o -o main
 
 #output the result of the program in plain text then copy that over to a text file named result.txt
 #echo "$(./exam)" | cat > result.txt
 
 #print out the results
-./test
+./main
 
 #delete the object files
 rm *.o
